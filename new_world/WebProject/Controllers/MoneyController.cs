bcac3ec8ed9b7edf20b841509cd270ebe7a1fe48
SamuraiKeksik <67840425+SamuraiKeksik@users.noreply.github.com>
2023-01-09 21:31:00 +0300

    Transaction
    Transaction between users

---
+++ new_world/WebProject/Controllers/MoneyController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebProject.Models;
+using WebProject.Models.ViewModels;
+
+namespace WebProject.Controllers
+{
+    [Authorize]
+    public class MoneyController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly UserManager<AppUser> _userManager;
+
+        public MoneyController(AppDbContext dbContext, UserManager<AppUser> userManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+
+        public IActionResult Transaction()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Transaction(TransactionModel transaction)
+        {
+            string userId = transaction.userId;
+            int money = transaction.money;
+            var currentUser = await _userManager.FindByNameAsync(User.Identity.Name);
+            var anotherUser = await _userManager.FindByIdAsync(userId);
+            if (anotherUser == null)
+                return View();
+            if (currentUser.Money < money)
+                return View();
+
+            currentUser.Money -= money;
+            anotherUser.Money += money;
+            await _userManager.UpdateAsync(currentUser);
+            await _userManager.UpdateAsync(anotherUser);
+            return View();
+
+        }
+    }
+}
---
+++ new_world/WebProject/Models/ViewModels/TransactionModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebProject.Models.ViewModels
+{
+    public class TransactionModel
+    {
+        public string userId { get; set; }
+        public int money { get; set; }
+    }
+}
